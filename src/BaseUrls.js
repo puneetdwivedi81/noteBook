7f9e47b8aefcfa548249eeabfd4c71a63fe36e9e
puneetdwivedi81 <dwivedipuneet29@gmail.com>
2026-03-19 12:46:24 +0530

    base_url change

--- src/BaseUrls.js
+++ src/BaseUrls.js
@@ -1,5 +1,5 @@
 
-export const BaseUrls = "http://localhost:8000/api/v3.2"
-// export const BaseUrls = "https://itsnotebook.onrender.com/api/v3.2"
+// export const BaseUrls = "http://localhost:8000/api/v3.2"
+export const BaseUrls = "https://notebook-vith.onrender.com/api/v3.2"
 
 // http://localhost:8000/api/v3.2/auth/login
